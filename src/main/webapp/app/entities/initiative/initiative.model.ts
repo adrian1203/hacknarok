5a4e4d64d6933fe55365467e7dd419ae5f8f687e
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-04-06 17:11:24 +0200

    initiative

---
+++ src/main/webapp/app/entities/initiative/initiative.model.ts
@@ -0,0 +1,15 @@
+export class Event {
+    id?: any;
+    login?: string;
+    firstName?: string;
+    lastName?: string;
+    email?: string;
+    activated?: boolean;
+    langKey?: string;
+    authorities?: any[];
+    createdBy?: string;
+    createdDate?: Date;
+    lastModifiedBy?: string;
+    lastModifiedDate?: Date;
+    password?: string;
+}
