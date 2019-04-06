f1d5753d33aca6899d08fa79d9aeab9aa208f784
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-04-06 21:17:45 +0200

    add initiative

--- src/main/webapp/app/entities/initiative/initiative-add.component.ts
+++ src/main/webapp/app/entities/initiative/initiative-add.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { InitiativeService } from 'app/entities/initiative/initiative.service';
 import { Event } from 'app/entities/initiative/initiative.model';
+import { User } from 'app/core';
 
 @Component({
     selector: 'jhi-initiative-add',
@@ -9,13 +10,39 @@ import { Event } from 'app/entities/initiative/initiative.model';
 })
 export class InitiativeAddComponent implements OnInit {
     event: Event;
+    creator: User;
+    createDate: Date;
+    startDate: Date;
+    endDate: Date;
+    latitude: number;
+    longtitude: number;
+    title: string;
+    description: string;
+    rating: number;
+    isSaving: boolean;
+
     constructor(private initiativeService: InitiativeService) {}
 
     ngOnInit() {
+        this.isSaving = false;
         this.event = new Event();
+        this.creator = new User();
+        this.createDate = new Date();
+        this.startDate = new Date();
+        this.endDate = new Date();
+        this.latitude = 0.0;
+        this.longtitude = 0.0;
+        this.title = '';
+        this.description = '';
+        this.rating = 0.0;
+    }
+
+    previousState() {
+        window.history.back();
     }
 
     public save() {
         this.initiativeService.createEvent(this.event);
+        console.log('dupka');
     }
 }
