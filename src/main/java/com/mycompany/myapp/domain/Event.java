e51c5f282fbf8305326cc8302e573319e225275f
bartoszgaj <bartosz.gaj4@gmail.com>
2019-04-06 17:09:21 +0200

    Added Event Entity

---
+++ src/main/java/com/mycompany/myapp/domain/Event.java
@@ -0,0 +1,50 @@
+package com.mycompany.myapp.domain;
+
+import org.hibernate.annotations.Cache;
+import org.hibernate.annotations.CacheConcurrencyStrategy;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+import java.io.Serializable;
+import java.util.Date;
+
+@Entity
+@Table(name = "event")
+public class Event extends AbstractAuditingEntity implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
+    private Long id;
+
+    @OneToOne
+    private User creator;
+
+    @Column(name = "adddate")
+    private Date createDate;
+
+    @Column(name = "startdate")
+    private Date startDate;
+
+    @Column(name = "enddate")
+    private Date endDate;
+
+    @Column(name = "latitude")
+    private Double latitude;
+
+    @Column(name = "longitude")
+    private Double longitude;
+
+    @Size(max = 256)
+    @Column(name = "title", length = 256)
+    private String title;
+
+
+    @Size(max = 256)
+    @Column(name = "description", length = 256)
+    private String description;
+
+    @Column(name = "rating")
+    private Double rating;
+}
