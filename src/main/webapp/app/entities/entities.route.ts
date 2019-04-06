90a9f4d5eaa31bda08093b154829e4722e2b3d87
Jan Ignatowicz <ignatowiczjan1@gmail.com>
2019-04-06 18:03:02 +0200

    initiative route

--- src/main/webapp/app/entities/entities.route.ts
+++ src/main/webapp/app/entities/entities.route.ts
@@ -1,9 +1,10 @@
 import { UserRouteAccessService } from 'app/core';
 import { Routes } from '@angular/router';
-import { InitiativeDetailRoute, InitiativeRoute } from 'app/entities/initiative/initiative.route';
+import { InitiativeDetailRoute, InitiativeRoute, InitiativeAddRoute } from 'app/entities/initiative/initiative.route';
 
 const INITIATIVE_ROUTES = [InitiativeRoute];
 const INITIATIVE_DETAIL_ROUTES = [InitiativeDetailRoute];
+const INITIATIVE_ADD_ROUTES = [InitiativeAddRoute];
 
 export const entitiesState: Routes = [
     {
@@ -15,5 +16,10 @@ export const entitiesState: Routes = [
         path: '',
         canActivate: [UserRouteAccessService],
         children: INITIATIVE_DETAIL_ROUTES
+    },
+    {
+        path: '',
+        canActivate: [UserRouteAccessService],
+        children: INITIATIVE_ADD_ROUTES
     }
 ];
--- src/main/webapp/app/entities/initiative/initiative.route.ts
+++ src/main/webapp/app/entities/initiative/initiative.route.ts
@@ -2,6 +2,7 @@ import { Route } from '@angular/router';
 import { JhiMetricsMonitoringComponent } from 'app/admin';
 import { InitiativeComponent } from 'app/entities/initiative/initiative.component';
 import { InitiativeDetailComponent } from 'app/entities/initiative/initiative-detail.component';
+import { InitiativeAddComponent } from 'app/entities/initiative/initiative-add.component';
 
 export const InitiativeRoute: Route = {
     path: 'initiative',
@@ -14,6 +15,14 @@ export const InitiativeRoute: Route = {
 export const InitiativeDetailRoute: Route = {
     path: 'initiative/:id',
     component: InitiativeDetailComponent,
+    data: {
+        pageTitle: 'Inicjatywy'
+    }
+};
+
+export const InitiativeAddRoute: Route = {
+    path: 'initiative/:id',
+    component: InitiativeAddComponent,
     data: {
         pageTitle: 'Inicjatywy'
     }
