012bb4708f3559758f8ca5a329813027d6d950a7
agnieszka <agapekala1@wp.pl>
2019-04-07 10:40:51 +0200

    index view

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -7,7 +7,7 @@ import { LoginModalService, AccountService, Account } from 'app/core';
 @Component({
     selector: 'jhi-home',
     templateUrl: './home.component.html',
-    styleUrls: ['home.scss']
+    styleUrls: ['home.scss', 'home1.css']
 })
 export class HomeComponent implements OnInit {
     account: Account;
